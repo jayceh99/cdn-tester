74ed36cf5a8794f524a1a7079c7b9658bb1b0902
jayce <louis821110@gmail.com>
2023-05-11 22:23:03 +0800

    update

--- cdn_tester.py
+++ cdn_tester.py
@@ -11,19 +11,22 @@ def _make_request(self,conn,method,url,**kwargs):
         setattr(response,'peer',None)
     return response
 
-HTTPConnectionPool._old_make_request = HTTPConnectionPool._make_request
-HTTPConnectionPool._make_request = _make_request
-
-
-
-r = requests.get('https://video.cloud.edu.tw/video/co_video_content.php?p=401467')
-#r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/manifest.mpd')
-ip_port = (r.raw._original_response.peer)
-ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
-print (ip_port[0]+':'+ip_port[1])
-#print(r.content)
-r = requests.get(r'https://whois.tanet.edu.tw/showWhoisPublic.php?queryString='+str(ip_port[0])+'&submit=%E9%80%81%E5%87%BA')
-data = html.fromstring(r.content.decode('UTF-8'))
-max = len(data.xpath('/html/body/center/table[2]/tr'))
-for i in range(1,max):
-    print(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()'))
+def main():
+    HTTPConnectionPool._old_make_request = HTTPConnectionPool._make_request
+    HTTPConnectionPool._make_request = _make_request
+    r = requests.get('https://video.cloud.edu.tw/video/co_video_content.php?p=401467')
+    #r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/chunk_ctaudio_ridp0aa0br127435_cs3175421_mpd.m4s')
+    ip_port = (r.raw._original_response.peer)
+    ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
+    print (ip_port[0]+':'+ip_port[1])
+    r = requests.get(r'https://whois.tanet.edu.tw/showWhoisPublic.php?queryString='+str(ip_port[0])+'&submit=%E9%80%81%E5%87%BA')
+    data = html.fromstring(r.content.decode('UTF-8'))
+    max = len(data.xpath('/html/body/center/table[2]/tr'))
+    for i in range(1,max):
+        if '網段' in str(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()')) :
+            print('------------------------------------------')
+        print(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()'))
+    print('------------------------------------------')
+    
+if __name__ == '__main__':
+    main()
