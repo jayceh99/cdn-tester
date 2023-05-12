4ab736f899a0e4838ace0e7a1b7a96b28971b62d
jayce <louis821110@gmail.com>
2023-05-12 22:44:30 +0800

    update

--- cdn_tester.py
+++ cdn_tester.py
@@ -1,24 +1,14 @@
 import requests
 from lxml import html
-from requests.packages.urllib3.connectionpool import HTTPConnectionPool
 
-def _make_request(self,conn,method,url,**kwargs):
-    response = self._old_make_request(conn,method,url,**kwargs)
-    sock = getattr(conn,'sock',False)
-    if sock:
-        setattr(response,'peer',sock.getpeername())
-    else:
-        setattr(response,'peer',None)
-    return response
+def get_server_ip(url):
+    r = requests.get(url, stream=True)
+    print('local IP port is '+str(r.raw._connection.sock.getsockname()))
+    return str(r.raw._connection.sock.getpeername())
 
-def main():
-    HTTPConnectionPool._old_make_request = HTTPConnectionPool._make_request
-    HTTPConnectionPool._make_request = _make_request
-    r = requests.get('https://video.cloud.edu.tw/video/co_video_content.php?p=401467')
-    #r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/chunk_ctaudio_ridp0aa0br127435_cs3175421_mpd.m4s')
-    ip_port = (r.raw._original_response.peer)
+def get_server_organization(ip_port):
     ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
-    print (ip_port[0]+':'+ip_port[1])
+    print ('server IP port is '+ip_port[0]+':'+ip_port[1])
     r = requests.get(r'https://whois.tanet.edu.tw/showWhoisPublic.php?queryString='+str(ip_port[0])+'&submit=%E9%80%81%E5%87%BA')
     data = html.fromstring(r.content.decode('UTF-8'))
     max = len(data.xpath('/html/body/center/table[2]/tr'))
@@ -27,6 +17,10 @@ def main():
             print('------------------------------------------')
         print(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()'))
     print('------------------------------------------')
+
+def main():
+    url = 'https://video.cloud.edu.tw/video/co_video_content.php?p=401467'
+    get_server_organization(get_server_ip(url))
     
 if __name__ == '__main__':
     main()
