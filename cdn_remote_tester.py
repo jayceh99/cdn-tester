d86e6b3a3eea642975adfecf94341880691b89ce
jayce <louis821110@gmail.com>
2023-06-27 13:40:16 +0800

    update domain

--- cdn_remote_tester.py
+++ cdn_remote_tester.py
@@ -3,14 +3,16 @@ import clientsubnetoption
 import get_server_info
 #210.240.152.121
 #140.111.67.82
-client_ip = '140.128.53.34'
-#client_ip = '140.111.67.82'
+#client_ip = '140.128.53.34'
+#140.128.51.17
+client_ip = '120.125.82.161'
 domain = 'media.video.cloud.edu.tw'
+domain = 'mediavideocloudedutw.tanetcdn.edu.tw'
 edns_info = clientsubnetoption.ClientSubnetOption(client_ip)
 message = dns.message.make_query(domain, 'A')
 message.use_edns(options=[edns_info])
 r = dns.query.udp(message, '8.8.8.8')
-
+#r = dns.query.udp(message, '163.28.6.1')
 for answer in r.answer:
     if answer.rdtype == dns.rdatatype.A:
         for item in answer.items:
--- cdn_tester.py
+++ cdn_tester.py
@@ -13,6 +13,7 @@ class cdn_tester:
     def dns_get_server_ip(self):
         resolver = dns.resolver.Resolver()
         resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+        #resolver.nameservers = ['163.28.6.1', '140.111.233.5']
         answers = resolver.resolve(self.domain)
 
         for data in answers:
@@ -22,11 +23,18 @@ class cdn_tester:
 
     def httping(self):
         strat_time = time.time()
-        r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/manifest.mpd' , stream=True)
+        #r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2021/09/video_391972_1080.mp4/manifest.mpd')
+        r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/06/video_401669_1080.mp4/manifest.mpd')
+        #r = requests.get('https://media.video.cloud.edu.tw')
+        return str(int(((time.time() - strat_time)*1000))) +' ms'
+    ''''
         if r.status_code == 200 :
             return str(int(((time.time() - strat_time)*1000))) +' ms'
         else :
-            return 'test failed' 
+            #return ' test failed status code  %s' % (r.status_code) 
+            return str(int(((time.time() - strat_time)*1000))) +' ms'
+    '''
+        
         
     def get_client_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -48,122 +56,18 @@ class cdn_tester:
         ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
         return ip_port
         
-
-
-
-class cdn_tester1:
-    def __init__(self,domain):
-        self.domain = domain
-    
-    def dns_get_server_ip(self):
-        resolver = dns.resolver.Resolver()
-        resolver.nameservers = ['8.8.8.8', '8.8.4.4']
-        answers = resolver.resolve(self.domain)
-
-        for data in answers:
-            ip = str(data)
-        return ip 
-
-    def httping(self):
-        strat_time = time.time()
-        #r  = requests.get('https://'+self.domain , stream=True)
-        r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/manifest.mpd' , stream=True)
-        if r.status_code == 200 :
-            return str(int(((time.time() - strat_time)*1000))) +' ms' , str(r.raw._connection.sock.getsockname())
-        else :
-            return 'test failed' , str(r.raw._connection.sock.getsockname())
-    
-
-    def format_data(self , data):
-        formated_data = str(data).replace('[','').replace(']','').replace('\'','')
-        if '：' in formated_data:
-            formated_data = formated_data.split('：')
-            return formated_data
-        else:
-            return formated_data
-    
-    def format_ip_info(self , ip_port):
-        ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
-        return ip_port
-
-
-
-
-def get_server_ip_old(url):
-    r = requests.get(url, stream=True)
-    return str(r.raw._connection.sock.getpeername()) , str(r.raw._connection.sock.getsockname())
-
-
-def get_server_organization(ip_port):
-    server_ip , your_ip = (ip_port)
-    server_ip = format_ip_info(server_ip)
-    your_ip = format_ip_info(your_ip)
-    tb = pt.PrettyTable()
-    tb.field_names = ['Key','Value']
-    tb.add_row(['My IP Address',your_ip[0]])
-    tb.add_row(['Server IP Address',server_ip[0]])
-    r = requests.get(r'https://whois.tanet.edu.tw/showWhoisPublic.php?queryString='+str(server_ip[0])+'&submit=%E9%80%81%E5%87%BA')
-    data = html.fromstring(r.content.decode('UTF-8'))
-    max = len(data.xpath('/html/body/center/table[2]/tr'))
-
-
-    for i in range(1,max):
-        if '網段' in str(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()')) :
-            tmp_data  = format_data(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td/text()'))
-            key = format_data(str(tmp_data[0]))
-            value = format_data(str(tmp_data[1]))
-            tb.add_row(['-'*30,'-'*30])
-            tb.add_row([key,value])
-
-        if len(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td')) == 2 :
-            key = format_data(str(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td[1]/text()')))
-            value = format_data(str(data.xpath('/html/body/center/table[2]/tr['+str(i)+']/td[2]/text()')))
-            tb.add_row([key,value])
-    print(tb)
-
-def format_data(data):
-    formated_data = str(data).replace('[','').replace(']','').replace('\'','')
-    if '：' in formated_data:
-        formated_data = formated_data.split('：')
-        return formated_data
-    else:
-        return formated_data
-    
-
-def format_ip_info(ip_port):
-    ip_port = str(ip_port).replace('(','').replace(')',"").replace('\'','').replace(' ','').split(',')
-    return ip_port
-
-
 def main():
 
-    mode = input("select mode:\n")
-    domain = "media.video.cloud.edu.tw"
+    #domain = "media.video.cloud.edu.tw"
+    domain = "mediavideocloudedutw.tanetcdn.edu.tw"
+    cdn_tester_q = cdn_tester(domain)
+    get_server_ip = cdn_tester_q.dns_get_server_ip()
+    client_ip = cdn_tester_q.get_client_ip()
+    httping = cdn_tester_q.httping()
+    get_server_info.get_server_organization(domain , get_server_ip ,  client_ip , httping)
+    del cdn_tester_q , get_server_ip , client_ip , httping 
+
     
-
-    if mode == '1':
-    #url = 'https://video.cloud.edu.tw/video/co_video_content.php?p=401467'
-    #url = 'https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/manifest.mpd'
-        url = 'https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/04/video_401468_1440.mp4/chunk_ctvideo_ridp0va0br708614_cs5400000_mpd.m4s'
-        data = get_server_ip_old(url)
-        get_server_organization(data)
-          
-
-    elif mode == '2':
-        cdn_tester_q = cdn_tester(domain)
-        get_server_ip = cdn_tester_q.dns_get_server_ip()
-        client_ip = cdn_tester_q.get_client_ip()
-        httping = cdn_tester_q.httping()
-        get_server_info.get_server_organization(domain , get_server_ip ,  client_ip , httping)
-
-    elif mode == '3':
-        cdn_tester_q = cdn_tester1(domain)
-        get_server_ip = cdn_tester_q.dns_get_server_ip()
-        httping , client_ip = cdn_tester_q.httping()
-        get_server_info.get_server_organization(domain , get_server_ip ,  client_ip , httping)
-    
-
-
 if __name__ == '__main__':
     main()
 
