7380b44bb703cb1b4d1014ee77ddc9344636669c
jayce <louis821110@hmail.com>
2023-07-10 15:28:29 +0800

    update

--- new_cdn_tester/cdn_tester.py
+++ new_cdn_tester/cdn_tester.py
@@ -6,17 +6,18 @@ import socket
 import time  
 import get_server_info
 import os
+import json
 class cdn_tester:
-    def __init__(self,domain,dns_name):
+    def __init__(self,domain,dns_name,requests_target):
         self.domain = domain
         self.dns = dns_name
+        self.requests_target = requests_target
         os.popen('netsh interface ip set dnsservers "乙太網路" static '+self.dns+' primary')
         #time.sleep(1)
 
     def dns_get_server_ip(self):
         resolver = dns.resolver.Resolver()
         resolver.nameservers = [self.dns]
-        #resolver.nameservers = ['163.28.6.1', '140.111.233.5']
         answers = resolver.resolve(self.domain)
 
         for data in answers:
@@ -25,18 +26,14 @@ class cdn_tester:
         return server_ip 
 
     def httping(self):
-        strat_time = time.time()
-        r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2021/09/video_391972_1080.mp4/manifest.mpd')
-        #r = requests.get('https://media.video.cloud.edu.tw/vod/_definst_/mp4:uploads.video/2023/06/video_401669_1080.mp4/manifest.mpd')
-        #r = requests.get('https://media.video.cloud.edu.tw')
-        return str(int(((time.time() - strat_time)*1000))) +' ms'
-    ''''
-        if r.status_code == 200 :
+        try:
+            r = requests.get(self.requests_target)
+            strat_time = time.time()
+            r = requests.get(self.requests_target)
             return str(int(((time.time() - strat_time)*1000))) +' ms'
-        else :
-            #return ' test failed status code  %s' % (r.status_code) 
-            return str(int(((time.time() - strat_time)*1000))) +' ms'
-    '''
+
+        except Exception:
+            return "Test Failed"
         
         
     def get_client_ip(self):
@@ -60,30 +57,19 @@ class cdn_tester:
         return ip_port
         
 def main():
-    
-    #domain = "media.video.cloud.edu.tw"
-    domain = "mediavideocloudedutw.tanetcdn.edu.tw"
-    '''
-    opt = input('select dns   cht  or  moe \n')
-    if opt  == 'cht':
-        dns_name = '168.95.1.1'
-
-
-    elif opt == 'moe':
-        dns_name = '140.111.34.135'
-    '''
-    
-    
-    dns_name_s = ['168.95.1.1','140.111.34.135']
-    for dns_name in dns_name_s:
+    j = open(r'C:\Users\jayce\cdn-tester\new_cdn_tester\config.json','r')
+    j = json.loads(j.read())
+    domain = j["domain"]
+    requests_target = j["requests_target"]
+    for dns_name in j['dns'] :
         os.popen('ipconfig/flushdns')
-        cdn_tester_q = cdn_tester(domain,dns_name)
+        cdn_tester_q = cdn_tester(domain,dns_name['ip'],requests_target)
         get_server_ip = cdn_tester_q.dns_get_server_ip()
         client_ip = cdn_tester_q.get_client_ip()
         httping = cdn_tester_q.httping()
-        get_server_info.get_server_organization(domain , get_server_ip ,  client_ip , dns_name=dns_name , httping=httping)
+        get_server_info.get_server_organization(domain , get_server_ip ,  client_ip , dns_name=dns_name['ip'] , httping=httping)
         del cdn_tester_q , get_server_ip , client_ip , httping 
-        time.sleep(90)
+
 
     
 if __name__ == '__main__':
